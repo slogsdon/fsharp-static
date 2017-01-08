8b60b002dcf6e433f4e01f0564beb0f31d48bf71
Shane Logsdon <shane@shanelogsdon.com>
2017-01-07 22:04:52 -0500

    abstract out duplication

--- src/WordPress/WordPress.fs
+++ src/WordPress/WordPress.fs
@@ -14,7 +14,7 @@ module WordPress =
   let private endpointBase = "/wp-json/wp/v2"
 
   /// Retrieves a remote resource asynchronously
-  let private getResponseBodyAsync (uri : Uri) : Async<string> =
+  let getResponseBodyAsync (uri : Uri) : Async<string> =
     async {
       use httpClient = new HttpClient()
       use! response = httpClient.GetAsync(uri) |> Async.AwaitTask
@@ -29,6 +29,7 @@ module WordPress =
       | false -> args
       | true -> Map.add "_embed" (String "1") args
 
+    // TODO: find a better way to do this
     let toQueryStringParam (k, v) =
       match v with
       | Int i -> sprintf "%s=%i" k i
@@ -58,28 +59,28 @@ module WordPress =
   let buildUri endpoint options =
     buildUriWithArgs endpoint (Map.ofList []) options
 
-  /// Wraps WordPress REST API (v2) endpoints for obtaining post
-  /// data from a WordPress installation
-  module Posts =
-    /// Gets all posts asynchronously with arguments
-    let getAllWithArgsAsync(args : Map<string, ApiArgument>) (options : Options)  : Async<Post list> =
-      buildUriWithArgs "/posts" args options
+  /// Constructs a resource request and serializes response
+  let inline getResourceWithArgsAsync (resource : string) (args : Map<string, ApiArgument>) (options : Options) =
+      buildUriWithArgs (sprintf "/%s" resource) args options
       |> (match options.apiClient with
           | Default -> getResponseBodyAsync
           | Func fn -> fn)
       |> Async.map(Json.parse >> Json.deserialize)
+
+  /// Wraps WordPress REST API (v2) endpoints for obtaining post
+  /// data from a WordPress installation
+  module Posts =
+    /// Gets all posts asynchronously with arguments
+    let getAllWithArgsAsync args options : Async<Post list> =
+      getResourceWithArgsAsync "posts" args options
 
     /// Gets all posts asynchronously
     let getAllAsync options =
       getAllWithArgsAsync (Map.ofList []) options
 
     /// Gets a single post by `id` asynchronously with arguments
-    let getSingleWithArgsAsync (id : int) (args : Map<string, ApiArgument>) (options : Options) : Async<Post> =
-      buildUriWithArgs (sprintf "/posts/%i" id) args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getSingleWithArgsAsync id args options : Async<Post> =
+      getResourceWithArgsAsync (sprintf "posts/%i" id) args options
 
     /// Gets a single post by `id` asynchronously
     let getSingleAsync id options =
@@ -89,24 +90,16 @@ module WordPress =
   /// data from a WordPress installation
   module Pages =
     /// Gets all pages asynchronously with arguments
-    let getAllWithArgsAsync(args : Map<string, ApiArgument>) (options : Options)  : Async<Post list> =
-      buildUriWithArgs "/pages" args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getAllWithArgsAsync args options : Async<Post list> =
+      getResourceWithArgsAsync "pages" args options
 
     /// Gets all pages asynchronously
     let getAllAsync options =
       getAllWithArgsAsync (Map.ofList []) options
 
     /// Gets a single page by `id` asynchronously with arguments
-    let getSingleWithArgsAsync (id : int) (args : Map<string, ApiArgument>) (options : Options) : Async<Post> =
-      buildUriWithArgs (sprintf "/pages/%i" id) args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getSingleWithArgsAsync id args options : Async<Post> =
+      getResourceWithArgsAsync (sprintf "pages/%i" id) args options
 
     /// Gets a single page by `id` asynchronously
     let getSingleAsync id options =
@@ -116,24 +109,16 @@ module WordPress =
   /// data from a WordPress installation
   module Tags =
     /// Gets all tags asynchronously with arguments
-    let getAllWithArgsAsync(args : Map<string, ApiArgument>) (options : Options)  : Async<Tag list> =
-      buildUriWithArgs "/tags" args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getAllWithArgsAsync args options : Async<Tag list> =
+      getResourceWithArgsAsync "tags" args options
 
     /// Gets all tags asynchronously
     let getAllAsync options =
       getAllWithArgsAsync (Map.ofList []) options
 
     /// Gets a single tag by `id` asynchronously with arguments
-    let getSingleWithArgsAsync (id : int) (args : Map<string, ApiArgument>) (options : Options) : Async<Tag> =
-      buildUriWithArgs (sprintf "/tags/%i" id) args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getSingleWithArgsAsync id args options : Async<Tag> =
+      getResourceWithArgsAsync (sprintf "tags/%i" id) args options
 
     /// Gets a single tag by `id` asynchronously
     let getSingleAsync id options =
@@ -143,24 +128,16 @@ module WordPress =
   /// data from a WordPress installation
   module Categories =
     /// Gets all categories asynchronously with arguments
-    let getAllWithArgsAsync(args : Map<string, ApiArgument>) (options : Options)  : Async<Category list> =
-      buildUriWithArgs "/categories" args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getAllWithArgsAsync args options : Async<Category list> =
+      getResourceWithArgsAsync "categories" args options
 
     /// Gets all categories asynchronously
     let getAllAsync options =
       getAllWithArgsAsync (Map.ofList []) options
 
     /// Gets a single category by `id` asynchronously with arguments
-    let getSingleWithArgsAsync (id : int) (args : Map<string, ApiArgument>) (options : Options) : Async<Category> =
-      buildUriWithArgs (sprintf "/categories/%i" id) args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getSingleWithArgsAsync id args options : Async<Category> =
+      getResourceWithArgsAsync (sprintf "categories/%i" id) args options
 
     /// Gets a single category by `id` asynchronously
     let getSingleAsync id options =
@@ -170,24 +147,16 @@ module WordPress =
   /// data from a WordPress installation
   module Users =
     /// Gets all users asynchronously with arguments
-    let getAllWithArgsAsync(args : Map<string, ApiArgument>) (options : Options)  : Async<User list> =
-      buildUriWithArgs "/users" args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getAllWithArgsAsync args options : Async<User list> =
+      getResourceWithArgsAsync "users" args options
 
     /// Gets all users asynchronously
     let getAllAsync options =
       getAllWithArgsAsync (Map.ofList []) options
 
     /// Gets a single user by `id` asynchronously with arguments
-    let getSingleWithArgsAsync (id : int) (args : Map<string, ApiArgument>) (options : Options) : Async<User> =
-      buildUriWithArgs (sprintf "/users/%i" id) args options
-      |> (match options.apiClient with
-          | Default -> getResponseBodyAsync
-          | Func fn -> fn)
-      |> Async.map(Json.parse >> Json.deserialize)
+    let getSingleWithArgsAsync id args options : Async<User> =
+      getResourceWithArgsAsync (sprintf "users/%i" id) args options
 
     /// Gets a single user by `id` asynchronously
     let getSingleAsync id options =
